f702374450447978fa6b7b76057fa4811917f697
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-09-01 17:27:55 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,27 @@ public class Main {
             System.out.print("Ange pris: ");
             int price = scanner.nextInt();
 
-            
-        }
+            if (price >= 0) {
+                products[0] = price * 3;
+                products[1] = products[0] / 2 + 1;
+                products[2] = products[1] * 2;
+
+                while (loop <= 2) {
+                    System.out.print("Ange en siffra: ");
+                    int siffra = scanner.nextInt();
+                    products[siffra] = price;
+                    loop++;
+                }
+
+                System.out.println("prodA har priset " + products[2]);
+                System.out.println("prodB har priset " + products[0]);
+                System.out.println("prodC har priset " + products[1]);
+            } else {
+                System.out.println("Pris måste vara ett positivt heltal.");
+            }
+
+
+        } else {}
     }
 
 
