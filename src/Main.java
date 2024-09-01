e7a177eea52e8ba9082a259cc796c7821bb051ce
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-09-01 17:13:58 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Copyright: Khatch Shah
  * */
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Jesus Christ is the Lord †");
-    }
+    Scanner scanner = new Scanner(System.in);
+    int[] products = new int[3];
+    int loop = 0;
+
+    //System.out.println();
+
 }
